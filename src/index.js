72cdc2fbce235499b3a33ffa163cc76e06855945
roman.mits <roman.mitskevich1@mail.ru>
2022-02-13 23:46:33 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   return String(Math.abs(n)).split('').reverse().join('');
+
+   // let reversedString = '';
+   // for (let i = String(Math.abs(n)).length - 1; i >= 0; i--) {
+   //    reversedString += String(Math.abs(n))[i];
+   // }
+   // return reversedString;
 }
